c42051c60bf8c96e918c799ac0a3d492a2f27543
Saeid Behjat <saeiddeveloper@yahoo.com>
2025-05-28 23:56:12 +0330

    styled

--- dashboard.js
+++ dashboard.js
@@ -11,23 +11,23 @@ const renderUsers = (users) => {
     <div id="card">
       <h3>${user.id}</h3>
       <div>
-        <p><i class="fa-solid fa-user"></i> Name:</p>
+        <p><i class="fa-solid fa-user"></i> Name :</p>
         <span>${user.name.firstname} ${user.name.lastname}</span>
       </div>
       <div>
-        <p><i class="fa-solid fa-paperclip"></i> Username:</p>
+        <p><i class="fa-solid fa-paperclip"></i> Username :</p>
         <span>${user.username}</span>
       </div>
       <div>
-        <p><i class="fa-solid fa-envelope"></i> Email:</p>
+        <p><i class="fa-solid fa-envelope"></i> Email :</p>
         <span>${user.email}</span>
       </div>
       <div>
-        <p><i class="fa-solid fa-phone"></i> Phone:</p>
+        <p><i class="fa-solid fa-phone"></i> Phone :</p>
         <span>${user.phone}</span>
       </div>
       <div>
-        <p><i class="fa-solid fa-location-dot"></i> Address:</p>
+        <p><i class="fa-solid fa-location-dot"></i> Address :</p>
         <span>${user.address.city} - ${user.address.street} - ${user.address.zipcode} </span>
       </div>
     </div>
